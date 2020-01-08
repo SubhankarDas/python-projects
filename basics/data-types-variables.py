0f6fb6f4b5d1d1ecb8c974ad7b6254b882ea7ad7
Subhankar Das <subhankardas831@gmail.com>
2020-01-09 00:18:30 +0530

    Added data types and variables

---
+++ basics/data-types-variables.py
@@ -0,0 +1,21 @@
+#--- VARIABLES AND DATA TYPES ---#
+var1 = 10  # integer
+var2 = 12.34  # float
+var3 = 'John Doe'  # string
+var4 = True  # boolean
+
+print(var1)
+print(var2)
+print(var3)
+print(var4)
+
+del var1  # deletes reference to var1
+
+#--- STRING ---#
+str = var3  # string initialization
+
+print(str[1])  # character at index
+print(str[1:6])  # substring from 2nd char to 6th char
+print(str[2:])  # substring from 3rd char to last char
+print(str * 3)  # prints str 3 times
+print('Mr. ' + str)  # concat string
